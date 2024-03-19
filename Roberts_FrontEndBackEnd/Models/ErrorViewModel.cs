f5358b664aa2d144794f10ce93541ffeb8e60584
Rayleen Roberts <rayleen.roberts@student.centralia.edu>
2024-03-18 21:10:50 -0700

    Add project files.

---
+++ Roberts_FrontEndBackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Roberts_FrontEndBackEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
